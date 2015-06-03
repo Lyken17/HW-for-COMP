2f132e3813d29c5de8330947519d890f5b9d1202
ShiHou <tsuduku@hotmail.com>
2015-06-03 15:29:48 +0800

    add multi cpu

---
+++ Verilog/MultiCPU/alu.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:07:09 05/28/2015 
+// Design Name: 
+// Module Name:    alu 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu(in_A,in_B,alu_ctl,res
+    );
+input [31:0] in_A,in_B;
+input [1:0] alu_ctl;
+output reg [31:0] res;
+always @(alu_ctl or in_A or in_B)
+	case(alu_ctl)
+		2'b00 :res=in_A+in_B;
+		2'b01 :res=in_A-in_B;
+		2'b11 :res=in_A&in_B;
+		2'b10 :res=~(in_A|in_B);
+	endcase
+
+endmodule
---
+++ Verilog/MultiCPU/alu_wrapper.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:10:16 05/28/2015 
+// Design Name: 
+// Module Name:    alu_wrapper 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu_wrapper(rin_A,rin_B,off,ls,alu_ctl,res
+    );
+
+
+endmodule
